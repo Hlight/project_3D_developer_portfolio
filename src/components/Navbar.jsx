e3e85a3e301840a16a023943790ddb8c3f95b19e
Hlight <aaronostrowsky@gmail.com>
2024-04-11 15:29:47 -0700

    customize name and gitignore to allow for pages deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Aaron &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Aaron &nbsp;Ostrowsky
+            <span className='sm:block hidden'></span>
           </p>
         </Link>
 
